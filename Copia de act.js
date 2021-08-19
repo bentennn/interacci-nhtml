5545ccf3845feae5c8dcc6dba4002a6810a2462a
bentennn <87681300+bentennn@users.noreply.github.com>
2021-08-18 23:30:05 -0400

    Add files via upload

---
+++ Copia de act.js
@@ -0,0 +1,12 @@
+
+console.log(document.getElementById("ide4"))
+
+document.querySelector('h2')
+console.log(document.querySelector('h2'))
+console.log(document.querySelector('.lol'))
+
+const parrafo = document.getElementById('ide4')
+parrafo.textContent = 'actualización 2'
+parrafo.innerHTML = '<b>actualización 3</b>'
+
+ide4.classList.add('lol')
